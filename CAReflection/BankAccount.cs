ae4b40dc9a2a91006b7da1bef24d72d4fb32924d
Metigator <metigator@gmail.com>
2021-07-12 11:04:20 -0400

    initial commit

---
+++ CAReflection/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CAReflection
+{
+    public class BankAccount
+    {
+        private string accountNo;
+        private string holder;
+        private decimal balance;
+
+        public string AccountNo => accountNo;
+        public string Holder => holder;
+        public decimal Balance => balance;
+
+        public event EventHandler OnNegativeBalance;
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string accountNo, string holder, decimal balance)
+        {
+            this.accountNo = accountNo;
+            this.holder = holder;
+            this.balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            this.balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            this.balance -= amount;
+            if (this.balance < 0)
+                this.OnNegativeBalance.Invoke(this, null);
+        }
+
+        public override string ToString()
+        {
+            return $"{{ Account No.: {accountNo}, Holder: {holder}, Balance: ${balance}}}";
+        }
+
+    }
+}
---
+++ CAReflection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace CAReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\Youya\\Desktop\\MOE.dll";
+            // var path = @"C:\Users\Youya\Desktop\MOE.dll";
+            var assembly = Assembly.LoadFile(path);
+            var types = assembly.GetTypes();
+            foreach (var t in types)
+            {
+                Console.WriteLine(t);
+            }
+            Console.ReadKey();
+        }
+    } 
+}
